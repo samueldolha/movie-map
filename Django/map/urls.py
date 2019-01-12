c29b9892e5efaab84c26c346230f3a7d223227bf
unknown <swm1000@hotmail.com>
2019-01-12 16:49:54 -0700

    delete old files

--- Django/map/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('', views.index, name='index'),
-    path('api/map', views.MovieListCreate.as_view()),
-]
--- moviemap/map/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from map.models import Movie
-
-class MovieDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
