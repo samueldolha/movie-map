b28e8fdad5a12e4d9ba3e66aba443a34b66dda92
Samuel Dolha <dolha@ualberta.ca>
2019-01-11 18:57:36 -0700

    Create initial leaflet application

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    };
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng]
     return (
-      <div></div>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
