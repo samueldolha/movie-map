657c6e378172d6ee9a8096bf6ffe8db6355b7b0b
Taylor Zowtuk <zowtukt@gmail.com>
2019-01-12 14:44:15 -0700

    Added Models

--- moviemap/map/models.py
+++ moviemap/map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    pubDate = models.CharField(max_length=100, default='')
+    rating = models.CharField(max_length=5)
+
+
