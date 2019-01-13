8136a467a0749a14d97bc1f8e00f431b1a85c0da
Taylor Zowtuk <zowtukt@gmail.com>
2019-01-13 10:56:36 -0700

    Implement Heat Map

--- react/src/App.js
+++ react/src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet';
 import countryData from './countries.js'
 
+const data = { 1975: { Canada: .6 } }
+
+function getColor(p) {
+  return p > 1 ? '#800026' :
+           p > .85  ? '#BD0026' :
+           p > .70  ? '#E31A1C' :
+           p > .55  ? '#FC4E2A' :
+           p > .40   ? '#FD8D3C' :
+           p > .25  ? '#FEB24C' :
+           p > .10  ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+  return {
+    fillColor: getColor(data['1975'][feature.properties.ADMIN]),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  };
+}
+
 type State = {
   lat: number,
   lng: number,
@@ -28,7 +52,7 @@ export default class App extends Component<{}, State> {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-        <GeoJSON data={countryData}></GeoJSON>
+        <GeoJSON data={countryData} style={style}></GeoJSON>
       </Map>
     )
   }
