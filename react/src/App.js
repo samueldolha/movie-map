fe65df2f9f78166a8b48f2b757c5a7003e7b3d20
Taylor Zowtuk <zowtukt@gmail.com>
2019-01-12 20:05:14 -0700

    Develop React Map

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet';
+import countryData from './countries.js'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
-    };
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
+
+export default class App extends Component<{}, State> {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
   }
 
   render() {
@@ -16,17 +20,16 @@ class App extends Component {
     return (
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <GeoJSON data={countryData}></GeoJSON>
       </Map>
-    );
+    )
   }
 }
-
-export default App;
